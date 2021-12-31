9f1e28729edea0067bb93cb361dda6220e38bda6
rui-watanabe <wataru610806@gmail.com>
2021-12-31 13:14:16 +0900

    add Login page #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login'
 
 function App() {
   return (
     <div className="App">
       <main className="form-signin">
-        <form>
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-            <input type="email" className="form-control" placeholder="name@example.com" required />
-            <input type="password" className="form-control" placeholder="Password" required />
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        </form>
+        <Login />
       </main>
     </div>
   );
